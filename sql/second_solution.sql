3c23065d47f9ad7173e630f0c9a1cfcb00415c9b
lserra <laercio.serra@gmail.com>
2019-03-29 01:22:00 -0300

    second solution

---
+++ sql/second_solution.sql
@@ -0,0 +1,20 @@
+/*Testing the second solution*/
+-- (email)
+-- gender
+-- race/ethnicity => Don't exist the table race
+-- age
+-- income
+SELECT DISTINCT
+    -- a.sourceid as SOURCEID,
+    a.email as email,
+    b.entry as first_name,
+    upper(substr(a.entry, 1, 1)) as gender,
+    c.entry as age
+FROM 
+    "mm_redirect_logs"."mdb_field_gender" a 
+    INNER JOIN "mm_redirect_logs"."mdb_field_firstname" b
+    ON a.email = b.email AND a.sourceid = b.sourceid
+    
+    INNER JOIN "mm_redirect_logs"."mdb_field_age" c
+    ON a.email = c.email AND a.sourceid = c.sourceid
+-- WHERE a.sourceid = 1419;
