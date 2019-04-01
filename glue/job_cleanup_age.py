98c116dc3c8ef2dcea522d05de318f6d8e6c14c4
lserra <laercio.serra@gmail.com>
2019-04-01 20:16:16 -0300

    fixing the code

--- glue/job_cleanup_age.py
+++ glue/job_cleanup_age.py
@@ -11,46 +11,78 @@ from awsglue.dynamicframe import DynamicFrame
 from awsglue.job import Job
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
+
 from pyspark.context import SparkContext
-from pyspark.sql import SparkSession, SQLContext
-from pyspark.sql.functions import col, regexp_extract, regexp_replace, udf
-from pyspark.sql.types import StringType
+from pyspark.sql import SparkSession
 
 # Params to be trigged by lambda function
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
 # Create a Glue context
-glueContext = GlueContext(SparkContext.getOrCreate())
+sc = SparkContext()
+glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
 # Initiating job and args
 job = Job(glueContext)
-job.init(args['JOB_NAME'])
+job.init(args['JOB_NAME'], args)
 
 # Loading a table from Glue data catalog
 # email, age
-df_age = glueContext.create_dynamic_frame.from_catalog(
+datasource0 = glueContext.create_dynamic_frame.from_catalog(
     database="mm_redirect_logs",
-    table_name="new_age"
-    ).toDF()
+    table_name="new_age",
+    transformation_ctx="datasource0")
 
-# Lowering the case for columns to avoid Hive issues
-for col in df_age.columns:
-    df_age = df_age.withColumnRenamed(col, col.lower()).collect()
+applymapping1 = ApplyMapping.apply(
+    frame=datasource0,
+    mappings=[
+        ("email", "string", "email", "string"),
+        ("entry", "string", "entry", "string")
+        ],
+    transformation_ctx="applymapping1")
 
-# Dropping column
-df_age.drop('sourceid')
+selectfields2 = SelectFields.apply(
+    frame=applymapping1,
+    paths=["email", "entry"],
+    transformation_ctx="selectfields2"
+    )
 
-# Selecting distinct values
-df_age_unique = df_age.select('email', 'entry').distinct().collect()
+resolvechoice3 = ResolveChoice.apply(
+    frame=selectfields2,
+    choice="MATCH_CATALOG",
+    database="mm_data_lake",
+    table_name="mdb_field_new_age",
+    transformation_ctx="resolvechoice3"
+    )
+
+resolvechoice4 = ResolveChoice.apply(
+    frame=resolvechoice3,
+    choice="make_struct",
+    transformation_ctx="resolvechoice4"
+    )
+
+# Dropping fields with NULL values
+results1 = DropNullFields.apply(
+    frame=resolvechoice4,
+    transformation_ctx="results1"
+    )
 
 # Renaming column from entry to age
-df_age_unique.withColumnRenamed('entry', 'age').collect()
+results2 = RenameField.apply(
+    frame=results1,
+    old_name="entry",
+    new_name="age",
+    transformation_ctx="results2"
+    )
+
+# Selecting distinct values to put all data into a single file, 
+# We need to convert it to a data frame, repartition it, and write it out.
+results3 = results2.select_fields(
+    ['email', 'age']).toDF().distinct().repartition(1)
 
 # Converting to a dynamic dataframe
-df_dyf = DynamicFrame.fromDF(
-    df_age_unique, glueContext, "dynamic"
-    )
+df_dyf = DynamicFrame.fromDF(results3, glueContext, "dynamic")
 
 # Writing parquet format to load on Data Catalog
 glueContext.write_dynamic_frame.from_options(
