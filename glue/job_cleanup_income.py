9a49cf2907e3c8decec557e629018a6662d8285c
lserra <laercio.serra@gmail.com>
2019-04-01 21:26:28 -0300

    fixing the code

--- glue/job_cleanup_income.py
+++ glue/job_cleanup_income.py
@@ -48,20 +48,6 @@ selectfields2 = SelectFields.apply(
     transformation_ctx="selectfields2"
     )
 
-# resolvechoice3 = ResolveChoice.apply(
-#     frame=selectfields2,
-#     choice="MATCH_CATALOG",
-#     database="mm_data_lake",
-#     table_name="mdb_field_new_netincome",
-#     transformation_ctx="resolvechoice3"
-#     )
-
-# resolvechoice4 = ResolveChoice.apply(
-#     frame=resolvechoice3,
-#     choice="make_struct",
-#     transformation_ctx="resolvechoice4"
-#     )
-
 # Dropping fields with NULL values
 results1 = DropNullFields.apply(
     frame=selectfields2,
